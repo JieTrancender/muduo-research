aafc20c4d8e7908e5ff65cea6b470eb434a8168e
Jason <jie-email@jie-trancender.org>
2017-06-09 12:01:24 +0800

    add FileUtil

---
+++ base/FileUtil.h
@@ -0,0 +1,8 @@
+#ifndef _BASE_FILEUTIL_H
+#define _BASE_FILEUTIL_H
+
+#include <muduo/base/StringPiece.h>
+// #include <boost/noncopyable.hpp>
+#include "../core/noncopyable.hpp"
+
+#endif  //_BASE_FILEUTIL_H
